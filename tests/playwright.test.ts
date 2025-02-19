834b525ec270f1da441031c8f33aaeea49a43f45
chrissbaumann <mail@christianbaumann.dev>
2025-02-19 18:01:44 +0100

    Update assertions to check if title contains 'Playwright'

--- tests/playwright.test.ts
+++ tests/playwright.test.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('Check Playwright page title', async ({ page }) => {
     await page.goto('https://playwright.dev/');
-    // await expect(page).toHaveTitle('Playwright');
-    await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
+
+    await expect(page).toHaveTitle(/Playwright/);
 });
