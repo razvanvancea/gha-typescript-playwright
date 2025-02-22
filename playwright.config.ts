2420983396858983ec79fc2037edd156618987c4
chrissbaumann <mail@christianbaumann.dev>
2025-02-22 10:42:45 +0100

    Add HTML reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from '@playwright/test';
+import {defineConfig} from '@playwright/test';
 
 export default defineConfig({
+    reporter: [
+        ['html']
+    ],
     use: {
         headless: true,
         screenshot: 'on',
